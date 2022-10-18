c89b54ae75f6beea9f29b02ff7b0605953344ab6
Jonathan-T1 <jdcaon85@misena.edu.co>
2022-10-18 18:30:56 +0000

    first commit

---
+++ jonathan_java/Retospoo/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Scanner leer=new Scanner(System.in);
+
+
+        leer.close();
+
+    }
+}
